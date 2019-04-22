c5e77a2267ad9bcd7f231ded19ae6d9072c7cd6a
Mark Stosberg <mark@rideamigos.com>
2019-04-22 16:38:30 -0400

    WIP: failing tests for testing STDERR capturing.
    I believe the tests are failing because STDERR from the test is getting
    connecting with the parent process of Jest. Jest seems to already being
    doing some customized handling of STDERR so it's not properly captured.
    
    I think possible solutions are:
    
     1. Run the test in additinal layer of sub-process, so the parent is not
        Jest, but one we control.
     2. Instead of using "pipe" for testing, explicitly pass a stream or fd
        For stderr, which can be tested.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,17 @@
+import { shell } from "../lib/index"
+
 describe("shell()", () => {
-  it("example test", () => {
-    expect(true).toBe(true)
+  it("should not show stderr if not defined", () => {
+    expect(() => {
+      shell("boom")
+    }).toThrow("Command failed: boom")
+  })
+  it("should show stderr if defined", () => {
+    expect(() => {
+      shell("boom", { stdio: "pipe" })
+    }).toThrow("Command failed: boom")
+    expect(() => {
+      shell("boom", { stdio: "pipe" })
+    }).toThrow(/boom: not found/)
   })
 })
